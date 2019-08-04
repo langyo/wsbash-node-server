a0d2f3fbd13546aeacea646075c05402ccdc4d20
langyo <langyo.china@gmail.com>
2019-08-04 12:48:15 +0800

    修复先前的小错误

--- demo/example.js
+++ demo/example.js
@@ -2,5 +2,4 @@ const Server = require('../src/socketServer.js');
 
 let server = new Server(9233);
 
-server.register('test', () => console.log(test));
-console.log(server.commandRegister.registerObj);
+server.register('test', () => console.log('test'));
--- src/socketManager.js
+++ src/socketManager.js
@@ -14,7 +14,7 @@ module.exports = class SocketManager {
     }
 
     this.receive = (str) => {
-      console.log("Get the message from the server:", str);
+      console.log("Get the message from the client", this.id, ":", str);
 
       // Heart package check.
       if (str[0] == '@') return;
@@ -39,8 +39,8 @@ module.exports = class SocketManager {
             this.commandRegister.registerObj[obj.package](Object.assign({}, obj));
           } catch (e) {
             this.send({ type: 'system', package: 'error.runtime.execute', message: e.toString() });
-            break;
           }
+          break;
         case 'data':
           if (!this.commandRegister.receiveObj[obj.package]) {
             this.send({ type: 'system', package: 'error.package', message: 'Unknown package: ' + obj.package });
@@ -50,8 +50,8 @@ module.exports = class SocketManager {
             this.commandRegister.receiveObj[obj.package](Object.assign({}, obj));
           } catch (e) {
             this.send({ type: 'system', package: 'error.runtime.data', message: e.toString() });
-            break;
           }
+          break;
         case 'system':
           console.error('The client', this.id, 'has reported an system information as', obj.package, ':', obj.message);
           break;
